0841c3a5f53248e84f00286536b5401ffced8984
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-28 16:03:32 +0530

    added variables

--- digitalDCfilter.ino
+++ digitalDCfilter.ino
@@ -8,9 +8,8 @@ Acknowledgements duly noted.
 */
 int sample = 0;
 int last_sample = 0;
-double a = 0;
-
 double filtered_value = 0;
+double last_filtered_value = 0;
 
 void setup()
 {
@@ -23,7 +22,9 @@ void loop()
   last_sample = sample;
   sample = analogRead(A0);      // read in the next value
   
-  // Floating maths implementation of high pass filter takes 36-40 microseconds
+  last_filtered_value = filtered_value;
+  
+  // LPF 10Hz corner freq, IIR Butterworth 1st order
   filtered_value = 0.996 * (filtered_value + sample - last_sample);    
   
   //Serial.print(sample);
