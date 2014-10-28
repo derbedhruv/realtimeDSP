9fa694e62863894c5c15e14a887ac9a85c3a758a
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-10-28 20:17:59 +0530

    added the filter. its fine

--- digitalDCfilter.ino
+++ digitalDCfilter.ino
@@ -1,15 +1,11 @@
 /* 
-this has been taken and modified from
-
-http://openenergymonitor.org/emon/buildingblocks/digital-filters-for-offset-removal
-
-Acknowledgements duly noted.
-
+    this has been made by filter design assistance from http://www-users.cs.york.ac.uk/~fisher/cgi-bin/mkfscript
 */
 int sample = 0;
 int last_sample = 0;
 double filtered_value = 0;
 double last_filtered_value = 0;
+double GAIN = 4.077683537;
 
 void setup()
 {
@@ -18,17 +14,20 @@ void setup()
 
 void loop()
 {
-  // Generate a test signal
+  // We're going to read in samples from the analog port
+  // then process them and send through serial. 
+  // These wil be plotted on some kind of serial oscilloscope.
+  
   last_sample = sample;
-  sample = analogRead(A0);      // read in the next value
+  sample = analogRead(A0)/GAIN;      // read in the next value
   
   last_filtered_value = filtered_value;
   
   // LPF 10Hz corner freq, IIR Butterworth 1st order
-  filtered_value = 0.996 * (filtered_value + sample - last_sample);    
+  filtered_value = (sample + last_sample) + (0.5095254495*last_filtered_value);    
   
   //Serial.print(sample);
   //Serial.print(' ');
   Serial.println(filtered_value);
-  delay(10);
+  // delay(10);
 }
